bc22e36a9e3ddb41ee7e63000723922260f704ce
dogsinatas29 <dogsinatas29@gmail.com>
2026-07-16 21:21:02 +0900

    chore: remove accidentally committed 0-byte debug artifacts

--- scratch.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-console.log("Reading project_state.json...");
-const data = fs.readFileSync('/home/dogsinatas/TypeScript_project/antigravity-extension-vis/demo/data/project_state.json', 'utf-8');
-const state = JSON.parse(data);
-console.log(`Nodes: ${state.nodes.length}, Edges: ${state.edges.length}`);
--- test_scan.ts
+++
@@ -1,3 +0,0 @@
-import { BootstrapEngine } from './src/bootstrap/BootstrapEngine';
-const engine = new BootstrapEngine();
-engine.liteBootstrap('/home/dogsinatas/다운로드/AntennaPod-develop/AntennaPod').then(() => console.log('Done'));
